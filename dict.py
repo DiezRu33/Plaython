dbac684d6d4cb06f07fa938903c50b8c5b0a7759
MrPerson <diegoruffa@hotmail.com>
2023-01-14 14:46:35 -0300

    using conditions printing

--- dict.py
+++ dict.py
@@ -1,35 +1,56 @@
 
-dict = {}
-for i in range(1, 15):
-    dict[i] = i * 10
+# dict = {}
+# for i in range(1, 15):
+#     dict[i] = i * 10
 
-print(dict)
+# print(dict)
 
-dict_v2 = { i: i *2 for i in range(1,5 )}
-print(dict_v2)
+# dict_v2 = { i: i *2 for i in range(1,5 )}
+# print(dict_v2)
+
+# import random
+# countries = ['col', 'mex', 'arg', 'chi']
+# population = {}
+# for country in countries:
+#     population[country] = random.randint(1, 100)
+
+# print(population)
+
+# #short v
+# population_v2 = { country: random.randint(1, 100) for country in countries}
+# print(population_v2)
+
+
+# names = ['nico', 'zule', 'zeus']
+# ages = [12, 42, 232]
+
+# # zip une lista, list() lo convierte;
+# print(list(zip(names, ages)))
+
+# # short v para zip using dictionaries
+# new_dict = {
+#     name: age for (name, age) in zip(names, ages)
+# }
+
+# print(new_dict)
+
 
 import random
+
 countries = ['col', 'mex', 'arg', 'chi']
-population = {}
-for country in countries:
-    population[country] = random.randint(1, 100)
 
-print(population)
-
-#short v
 population_v2 = { country: random.randint(1, 100) for country in countries}
 print(population_v2)
 
+result = { 
+    country: population for (country, population) in population_v2.items() if { population > 50 }
+    
+}
+print(result)
 
-names = ['nico', 'zule', 'zeus']
-ages = [12, 42, 232]
-
-# zip une lista, list() lo convierte;
-print(list(zip(names, ages)))
-
-# short v para zip using dictionaries
-new_dict = {
-    name: age for (name, age) in zip(names, ages)
+text = 'abcdefghijklmnñopqrstuvwxyz'
+unique = {
+    c: c.upper() for c in text if c in 'aeiou'
 }
 
-print(new_dict)
+print(unique)
