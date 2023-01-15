db8bd8f8b6fbe3831f6ab9cc27692f83fec27dc1
MrPerson <diegoruffa@hotmail.com>
2023-01-15 19:43:59 -0300

    functions w/ python

---
+++ function.py
@@ -0,0 +1,22 @@
+print("Hola")
+
+def printe(var):
+    print(var * 2)
+    # print("Hola, este es el print 2")
+
+printe('Este es mi texto')
+printe('Hola')
+
+a = 10
+b = 90
+
+c = a + b
+
+def suma(a, b):
+    print(a + b)
+
+# suma(1, 5)
+
+# suma(20, 30)
+
+suma(c, 20)
