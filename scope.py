b2008dfd3239c2316526d98d37c3a8c1c9f49b2c
MR. Pers0n <diegoruffa@hotmail.com>
2023-01-15 23:23:15 -0300

    adding scopes

--- scope.py
+++ scope.py
@@ -22,7 +22,7 @@ def cuenta(n, n2, n3):
         sum3 += n + n2 + n3
         return sum, sum2, sum3
     
-print(cuenta(15, 100, 200))
+# print(cuenta(15, 100, 200))
 # cuenta(10, 50, 100)
         
     # print(res, res2)
@@ -36,3 +36,26 @@ print(cuenta(15, 100, 200))
 # print(cuenta(5 + 10 * 2))
     
 # print(cuenta(5 + 10 * 20))
+
+
+price = 100 # global (incapsulado)
+
+def increment():
+    price = 200
+    price = price - 50
+    print(price)
+    return price
+
+def other():
+    price = 400
+    result = 132
+    price3 = price + 50
+    print("El resultado es: ", result)
+    return "El precio es: ", price3 
+
+print(price)
+price2 = increment()
+print(price2)
+
+price3 = other()
+print(price3)
