69dce0e92aa69a6cda9df4c7d62adee6f0899ac9
MR. Pers0n <diegoruffa@hotmail.com>
2023-01-20 00:44:00 -0300

    adding RSP's game

--- rockscissorspaper.py
+++ rockscissorspaper.py
@@ -1,58 +1,77 @@
 import random
 
-options = ['piedra', 'papel', 'tijera']
+# options = ['piedra', 'papel', 'tijera']
 
-rounds = 1
-computer_wins = 0
-user_wins = 0
+# rounds = 1
+# computer_wins = 0
+# user_wins = 0
 
-while True:
+# def choose_options():
+#     user_option = input('piedra,papel o tijera => ')
+#     user_option = user_option.lower()
 
-    print("*" * 10)
-    print('ROUND', rounds)
-    print("*" * 10)
+#     rounds += 1
 
-    print('computer1_wins', computer_wins)
-    print('user_wins', user_wins)
-
-    user_option = input('piedra,papel o tijera => ')
-    user_option = user_option.lower()
-
-    rounds += 1
-
-    if not user_option in options:
-        print('esa opcion no es valida')
-        continue
+#     if not user_option in options:
+#         print('esa opcion no es valida')
     
-    computer_option = random.choice(options)
+#         computer_option = random.choice(options)
 
-    print('User option =>', user_option)
-    print('Computer option =>', computer_option)
 
-    if user_option == computer_option:
-        print("Empate!")
-    elif user_option == 'piedra':
-        if computer_option == 'tijera':
-            print('Piedra gana a tijera')
-            print('User ganó')
-            user_wins += 1
-        else:
-            print('Papel gana a piedra')
-            print("computer ganó")
-            computer_wins += 1
-    elif user_option == 'tijera':
-        if computer_option == 'papel':
-            print('tijera gana a papel')
-            print('user ganó')
-            user_wins += 1
-        else:
-            print('piedra gana a tijera')
-            print('computer ganó')
-            computer_wins += 1
-    if computer_wins == 2:
-        print('El ganador es la computadora')
-        break
+# while True:
+
+#     print("*" * 10)
+#     print('ROUND', rounds)
+#     print("*" * 10)
+
+#     print('computer1_wins', computer_wins)
+#     print('user_wins', user_wins)
+
+# if user_option == computer_option:
+#         print("Empate!")
+# elif user_option == 'piedra':
+#         if computer_option == 'tijera':
+#             print('Piedra gana a tijera')
+#             print('User ganó')
+#             user_wins += 1
+            
+# else:
+#         print('Papel gana a piedra')
+#         print("computer ganó")
+#         computer_wins += 1
+
+# if user_option == 'tijera':
+#         if computer_option == 'papel':
+#             print('tijera gana a papel')
+#             print('user ganó')
+#             user_wins += 1
+# else:
+#         print('piedra gana a tijera')
+#         print('computer ganó')
+#         computer_wins += 1
+# if computer_wins == 2:
+#         print('El ganador es la computadora')
+#         break
     
-    if user_wins == 2:
-        print('El ganador es User')
-        break
+# if user_wins == 2:
+#     print('El ganador es User')
+#     break
+
+#             print('User option =>', user_option)
+#             print('Computer option =>', computer_option)
+
+
+num = 100
+num1 = 100
+
+if num > num1:
+    print("El numero ", num, " es mayor que ",num1)
+elif num == num1:
+    print("El numero",num, "es malditamente identico a",num1)
+elif num < num1:
+    print("El",num, "es menor a ", num1)
+
+else:
+    print("El numero ", num , " es mayór que ", num1)
+
+foreach num>=num1
